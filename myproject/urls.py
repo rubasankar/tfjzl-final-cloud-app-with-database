47482cd6e145c7460287d7f7523510e4da6fb474
rubasankar <rubasankar2002@gmail.com>
2024-03-22 10:22:17 -0400

    final

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views import debug
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
+    path('',debug.default_urlconf),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
